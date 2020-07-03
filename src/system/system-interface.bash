5ef328fd2847c9f78e2b610fcf65ba68ced2856b
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-07-03 20:50:49 +0700

    Update system-interface.bash

--- src/system/system-interface.bash
+++ src/system/system-interface.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
 clear
-printf "System Manage by SKT Pro\n"
+printf "       -----------------------------\n"
+printf "        VPS MANAGE | `find /home -mindepth 1 -maxdepth 1 -type d | wc -l` domains\n"
+printf "       -----------------------------\n"
 printf "1. Install LEMP\n"
-printf "2. Update\n"
+printf "2. Update VPS\n"
 printf "My Select: (Ctril + C to Cancel) "
 read slc
 if [ ${slc} = 0 ]; then
 cd /root && ./install
 fi
 if [ ${slc} = 1 ]; then
+printf "Do you want to install LEMP? -Y/N\n"
+read qs
+if [ $qs = 'Y' ]; then
 cd /etc/skt.d/system && ./install-lemp.bash
+fi
 fi
 if [ ${slc} = 2 ]; then
 yum update -y --skip-broken
