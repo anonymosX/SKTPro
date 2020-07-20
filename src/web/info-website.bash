e285b3cdf5070bdaa357a2d7b75cf834f212dd93
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-07-20 16:17:14 +0700

    Update info-website.bash

--- src/web/info-website.bash
+++ src/web/info-website.bash
@@ -6,6 +6,7 @@ printf "                 Have `find /home -mindepth 1 -maxdepth 1 -type d | wc -
 printf "       -----------------------------\n" 
 printf "Option: \n"
 printf "1. Login                  2. Database\n"
+printf "\n"
 printf "Select: "
 read slc
 # Return Home
@@ -22,9 +23,14 @@ d=${D##*/}
 printf "* ${d}\n"
 fi
 done
-source /etc/skt.d/${d}/${d}.login
-printf "${d^^} login\n Username: ${wp_usr}\n Password: ${wp_pass}\n Email: ${e}\n"
+printf "Enter: "
+read enter
+printf "\n"
+source /etc/skt.d/${enter}/${enter}.login
+printf "${enter^^} login\n Username: ${wp_usr}\n Password: ${wp_pass}\n Email: ${e}\n"
+cd /etc/skt.d/web && ./info-interface.bash
 fi
+
 # Show Database
 if [ $slc = 2 ]; then
 printf "List domains:\n"
@@ -34,9 +40,14 @@ d=${D##*/}
 printf "* ${d}\n"
 fi
 done
-source /etc/skt.d/${d}/${d}.mariadb
-printf "${d^^}\nDatabase Name: ${dbn} \nUsername: ${dbu}\nUsername Password: ${dbp}\nRoot Password: ${mdbp}\n"
+printf "Enter: "
+read enter
+printf "\n"
+source /etc/skt.d/${enter}/${enter}.mariadb
+printf "${enter^^}\nDatabase Name: ${dbn} \nUsername: ${dbu}\nUsername Password: ${dbp}\nRoot Password: ${mdbp}\n"
+cd /etc/skt.d/web && ./info-interface.bash
 fi
+# Else
 if [ ${slc} != 0 -a ${slc} != 1 -a ${slc} != 2 ]; then
 clear
 cd /etc/skt.d/web && ./web-interface.bash
