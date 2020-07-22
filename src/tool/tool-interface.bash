3d3c4a86c96818cd78b92e7a8d8fb1149a5b235e
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-07-22 14:21:10 +0700

    Update tool-interface.bash

--- src/tool/tool-interface.bash
+++ src/tool/tool-interface.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 domain=https://raw.githubusercontent.com/anonymosX/SKTPro/master/src
 printf "       -----------------------------\n"
 printf "        SKT TOOL | `find /home -mindepth 1 -maxdepth 1 -type d | wc -l` domains\n"
