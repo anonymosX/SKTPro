90a96ccf73ede94b08c9bf74a0c57bfa5b4e3cff
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-07-29 20:02:28 +0700

    Update add.bash

--- src/web/add.bash
+++ src/web/add.bash
@@ -170,8 +170,9 @@ define('WP_DEBUG', false);
 define('FS_METHOD','direct');
 PHP
 
+#${d^^}&nbsp;|&nbsp;Online&nbsp;Store
 # INSTALL WORDPRESS
-wp core install --url=${d}  --title=${d^^}&nbsp;|&nbsp;Online&nbsp;Store --admin_user=${wp_usr} --admin_password=${wp_pass} --admin_email=$e --path=/home/${d}/public_html
+wp core install --url=${d}  --title=`${d^^} | Online Store` --admin_user=${wp_usr} --admin_password=${wp_pass} --admin_email=$e --path=/home/${d}/public_html
 
 # FIX ERROR INSTALLATION FAILED: COULD NOT CREATE DIRECTORY.
 #chmod 777 -R /home/${d}/public_html/wp-content
