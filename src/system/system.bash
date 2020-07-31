7119094ae9222a273009b8616d310e3681713596
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-07-31 20:22:26 +0700

    Update system.bash

--- src/system/system.bash
+++ src/system/system.bash
@@ -5,7 +5,8 @@ printf "        VPS MANAGE | `find /home -mindepth 1 -maxdepth 1 -type d | wc -l
 printf "       -----------------------------\n"
 printf "OPTIONS: \n"
 printf "1. INSTALL LEMP\n"
-printf "2. UPDATE VPS\n"
+printf "2. CLEAR MEMCACHED\n"
+printf "3. UPDATE VPS\n"
 printf "ENTER: "
 read enter
 if [ ${enter} = 0 ]; then
@@ -13,7 +14,10 @@ if [ ${enter} = 0 ]; then
 elif [ ${enter} = 1 ]; then
 	sh /etc/skt.d/tool/system/lemp.bash
 elif [ ${enter} = 2 ]; then
+	echo flush_all > /dev/tcp/127.0.0.1/11211	
+elif [ ${enter} = 3 ]; then
 	yum update -y --skip-broken
+
 else
 	sh /etc/skt.d/tool/system/system.bash
 fi
