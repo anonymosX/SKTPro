42195a0a401b812e32e716e31327ff5cd4c674a8
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-03 23:47:59 +0700

    update

--- src/system/lemp.bash
+++ src/system/lemp.bash
@@ -168,6 +168,62 @@ pagespeed EnableFilters insert_dns_prefetch;
 # rewrite CSS to load page-rendering CSS rules first.
 pagespeed EnableFilters prioritize_critical_css;
 EOF
+# Remove Default
+rm -f /etc/nginx/conf.d/default.conf
+cat > /etc/nginx/conf.d/default.conf<<"EOF"
+    #charset koi8-r;
+    #access_log  /var/log/nginx/host.access.log  main;
+
+    location / {
+        root   /usr/share/nginx/html;
+        index index.php index.html index.htm;
+        try_files $uri $uri/ /index.php?q=$uri&$args;
+    }
+
+    #error_page  404              /404.html;
+
+    # redirect server error pages to the static page /50x.html
+    #
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+
+    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+    #
+    #location ~ \.php$ {
+    #    proxy_pass   http://127.0.0.1;
+    #}
+
+    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+    #
+   location ~ \.php$ {
+        root           /usr/share/nginx/html;
+        fastcgi_pass   unix:/run/php-fpm/php-fpm.sock;
+        fastcgi_index  index.php;
+        fastcgi_param  SCRIPT_FILENAME   $document_root$fastcgi_script_name;
+        include        fastcgi_params;
+    }
+    #    root           html;
+    #    fastcgi_pass   127.0.0.1:9000;
+    #    fastcgi_index  index.php;
+    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+    #    include        fastcgi_params;
+    #}
+
+    # deny access to .htaccess files, if Apache's document root
+    # concurs with nginx's one
+    #
+    #location ~ /\.ht {
+    #    deny  all;
+    #}
+}
+EOF
+
+
+
+
+
 
 # MEMCACHED INSTALL
 printf "Installing Memcached\n"
@@ -188,8 +244,8 @@ cat > /root/.my.cnf<<"EOF"
 user=root
 password=SKTpWI5IexxF4oPenOYlOhJ
 EOF
-mdbp=SKTpWI5IexxF4oPenOYlOhJ
-printf "\nY\n${mdbp}\n${mdbp}\nY\nY\nY\nY\n" | mysql_secure_installation 
+source /root/.my.cnf
+printf "\nY\n${password}\n${password}\nY\nY\nY\nY\n" | mysql_secure_installation 
 clear
 # W-CLI INSTALL
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
