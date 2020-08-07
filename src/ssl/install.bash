618c56da5d598991431b95ba6e79365bc72fc26c
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-07 19:58:13 +0700

    Update install.bash

--- src/ssl/install.bash
+++ src/ssl/install.bash
@@ -1,5 +1,5 @@
-#printf "1\n${e}\nA\n1\n" | certbot --nginx -d ${d} -d www.${d}
-printf "${e}\nA\n1\n" | certbot --nginx -d ${d} -d www.${d}
+printf "1\n${e}\nA\n1\n" | certbot --nginx -d ${d} -d www.${d}
+#printf "${e}\nA\n1\n" | certbot --nginx -d ${d} -d www.${d}
 mkdir -p /etc/letsencrypt/live/${d}
 openssl dhparam 2048 -out /etc/letsencrypt/live/${d}/dhparam.pem
 mv /etc/nginx/conf.d/${d}.conf /etc/nginx/conf.d/${d}.conf.80
