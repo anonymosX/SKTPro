e371d94b74f9bb7b5c7e79f263daed8ae0c3a34d
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-10 00:21:00 +0700

    Update tool.bash

--- src/tool/tool.bash
+++ src/tool/tool.bash
@@ -7,29 +7,30 @@ printf "       -----------------------------\n"
 printf "OPTIONS: \n"
 printf "1. INSTALL\n"
 printf "2. UPDATE\n"
-printf "ENTER: "
-read enter
-clear
-if [ ${enter} = 0 ]; then
+printf "OPTION: "
+read OPTION
+
+if [ $OPTION = 0 ]; then
+	clear
 	sh /root/install
-elif [ ${enter} = 1 ]; then
+elif [ $OPTION = 1 ]; then
 {
 	yum install -y wget
 	mkdir -p /etc/skt.d/tool
 	cd /etc/skt.d/tool && mkdir -p web system ssl mariadb server domain
 	curl -N $url/config.txt | cat > /etc/skt.d/tool/config.txt
 	while IFS= read -r line; do 
-		curl -N ${url}/$line.bash | cat > /etc/skt.d/tool/$line.bash
+		curl -N $url/$line.bash | cat > /etc/skt.d/tool/$line.bash
 	done < "/etc/skt.d/tool/config.txt"
 	clear
 	printf "NINJA TOOL: INSTALLED\n"
 	sh /root/install
 }
-elif [ ${enter} = 2 ]; then
+elif [ $OPTION = 2 ]; then
 	curl -N $url/config.txt | cat > /etc/skt.d/tool/config.txt
 	cd /etc/skt.d/tool && rm -f web/* system/* ssl/* mariadb/* server/* domain/*
 	while IFS= read -r line; do 
-		curl -N ${url}/$line.bash | cat > /etc/skt.d/tool/$line.bash
+		curl -N $url/$line.bash | cat > /etc/skt.d/tool/$line.bash
 	done < "/etc/skt.d/tool/config.txt"
 	clear
 	printf "NINJA TOOl: UPDATED\n"
