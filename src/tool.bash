198b9778127b37d7e7b13df3bc425cc6a3a35818
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-11 12:28:33 +0700

    123

--- src/tool.bash
+++ src/tool.bash
@@ -15,24 +15,39 @@ if [ $OPTION = 0 ]; then
 	sh /root/install
 elif [ $OPTION = 1 ]; then
 {
-	yum install -y wget
 	mkdir -p /etc/skt.d/tool
-	cd /etc/skt.d/tool && mkdir -p web system ssl mariadb server domain
+	# DƠWNLOAD CONFIG FILE
+	curl -N $url/folder_config.txt | cat > /etc/skt.d/tool/folder_config.txt
+	curl -N $url/file_config.txt | cat > /etc/skt.d/tool/file_config.txt
+	# CREATE FOLDERS
+	while IFS= read -r line; do
+		mkdir -p /etc/skt.d/tool/$line
+	done < "/etc/skt.d/tool/folder_config.txt"
+
 	curl -N $url/config.txt | cat > /etc/skt.d/tool/config.txt
+	# CREATE FILES
 	while IFS= read -r line; do 
 		curl -N $url/$line.bash | cat > /etc/skt.d/tool/$line.bash
-	done < "/etc/skt.d/tool/config.txt"
+	done < "/etc/skt.d/tool/file_config.txt"
 	clear
+	printf "\n"
 	printf "NINJA TOOL: INSTALLED\n"
 	sh /root/install
 }
 elif [ $OPTION = 2 ]; then
-	curl -N $url/config.txt | cat > /etc/skt.d/tool/config.txt
-	cd /etc/skt.d/tool && rm -f web/* system/* ssl/* mariadb/* server/* domain/*
+	# UPDATE CONFIG FILE
+	curl -N $url/folder_config.txt | cat > /etc/skt.d/tool/folder_config.txt
+	curl -N $url/file_config.txt | cat > /etc/skt.d/tool/file_config.txt
+	# REFRESH FOLDERS
+	while IFS= read -r line; do
+		rm -f /etc/skt.d/tool/$line/*
+	done < "/etc/skt.d/tool/folder_config.txt"
+	# UPDATE FILES
 	while IFS= read -r line; do 
 		curl -N $url/$line.bash | cat > /etc/skt.d/tool/$line.bash
-	done < "/etc/skt.d/tool/config.txt"
+	done < "/etc/skt.d/tool/file_config.txt"
 	clear
+	printf "\n"
 	printf "NINJA TOOl: UPDATED\n"
 	sh /etc/skt.d/tool/tool.bash
 else 
