7047d3a574ac93c8ce92b62ca722a3ba5e257768
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-12 21:22:32 +0700

    Update updateAPI.bash

--- src/cloudflare/updateAPI.bash
+++ src/cloudflare/updateAPI.bash
@@ -6,11 +6,9 @@ printf " ==================================\n"
 printf "UPDATE NEW API\n"
 printf "CHOOSE ACCOUNT: "
 read NUMBER
-printf "\n"
 printf "ENTER NEW API: "
 read newAPI
-printf "\n"
-printf "DO YOU WANT TO UPDATE NEW API: $newAPI TO Cloudflare $NUMBER? - Y/N: "
+printf "DO YOU WANT TO UPDATE NEW API: $newAPI TO CLOUDFLARE $NUMBER? - Y/N: "
 read CONFIRM
 printf "\n"
 if   [ $CONFIRM = 0 ]; then
@@ -18,15 +16,16 @@ if   [ $CONFIRM = 0 ]; then
 elif [ $CONFIRM = 'Y' -o $CONFIRM = 'y' ]; then
 	clear
 	source /etc/skt.d/data/cloudflare/cloudflare_$NUMBER.txt
-	sed -i "s/$API/$newAPI/g" /etc/skt.d/data/cloudflare/cloudflare_$NUMBER.txt
 	for D in /home/*; do
 		if [ -d ${D} ]; then
 			DOMAIN=${D##*/}
-			if [ `sed -n '3p' /etc/skt.d/data/$DOMAIN/api_cf.txt` = $API ]; then
+			
+			if [ `sed -n '3p' /etc/skt.d/data/$DOMAIN/api_cf.txt` = ${CF_API} ]; then
 				sed -i "s+`sed -n '3p' /etc/skt.d/data/$DOMAIN/api_cf.txt`+$newAPI+g" /etc/skt.d/data/$DOMAIN/api_cf.txt
 			fi
 		fi
-	done
+	done	
+	sed -i "s/${CF_API}/$newAPI/g" /etc/skt.d/data/cloudflare/cloudflare_$NUMBER.txt
 	printf "UPDATE new API successful \n"
 elif [ $CONFIRM = 'N' -o $CONFIRM = 'n' ]; then
 	clear ; printf "Cancel: update API\n"
