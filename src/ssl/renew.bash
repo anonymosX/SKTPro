572dd4129b2a6116934f5910a2703b45f2e8a05a
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-08-28 15:41:25 +0700

    Update renew.bash

--- src/ssl/renew.bash
+++ src/ssl/renew.bash
@@ -20,9 +20,9 @@ elif [ $CONFIRM = 'Y' -o $CONFIRM = 'y' ]; then
 	clear
 	printf "YOU HAVE CHOOSE YES\n"
 	# PAUSE CLOUDFLARE - PAUSE ZONE ID
-	curl -X PATCH "https://api.cloudflare.com/client/v4/zones/`sed -n "3p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
-		 -H "X-Auth-Email: `sed -n "1p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
-		 -H "X-Auth-Key: `sed -n "2p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
+	curl -X PATCH "https://api.cloudflare.com/client/v4/zones/`sed -n "3p" /etc/skt.d/data/$DOMAIN/api_cf.txt`" \
+		 -H "X-Auth-Email: `sed -n "1p" /etc/skt.d/data/$DOMAIN/api_cf.txt`" \
+		 -H "X-Auth-Key: `sed -n "2p" /etc/skt.d/data/$DOMAIN/api_cf.txt`" \
 		 -H "Content-Type: application/json" \
 		 --data '{"paused":'true'}'	
 	# WAIT 60s TO A RECORD UPDATE
@@ -30,9 +30,9 @@ elif [ $CONFIRM = 'Y' -o $CONFIRM = 'y' ]; then
 	# CERTBOT RENEW - SSL
 	certbot renew --cert-name $DOMAIN
 	# ENABLE CLOUDFLARE AGAIN - ENABLE ZONE ID
-	curl -X PATCH "https://api.cloudflare.com/client/v4/zones/`sed -n "3p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
-		 -H "X-Auth-Email: `sed -n "1p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
-		 -H "X-Auth-Key: `sed -n "2p" /ect/skt.d/data/$DOMAIN/api_cf.txt`" \
+	curl -X PATCH "https://api.cloudflare.com/client/v4/zones/`sed -n "3p" /ecetct/skt.d/data/$DOMAIN/api_cf.txt`" \
+		 -H "X-Auth-Email: `sed -n "1p" /etc/skt.d/data/$DOMAIN/api_cf.txt`" \
+		 -H "X-Auth-Key: `sed -n "2p" /etc/skt.d/data/$DOMAIN/api_cf.txt`" \
 		 -H "Content-Type: application/json" \
 		 --data '{"paused":'false'}'		
 elif [ $CONFIRM = 'N' -o $CONFIRM = 'n' ]; then
