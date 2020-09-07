ad1c5255657bcf9960d60c1d884c1635be49f048
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-09-07 15:28:09 +0700

    Update rest_api.bash

--- src/web/rest_api.bash
+++ src/web/rest_api.bash
@@ -60,6 +60,8 @@ done < /root/track.txt
 
 elif [ $OPTION = 3 ]; then
 clear
+# REFRESH ORDER FILE
+rm -rf /root/orders.csv
 printf " #######################################\n"
 printf " Export Orders | REST API | WOOCOMMERCE\n"
 printf " #######################################\n"
@@ -73,34 +75,38 @@ curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders \
 	-d '{
 	"status": "processing"	
 	}' \ | python -m json.tool | jq -r ".[].id" | cat > /root/export_orders_${WOOCOMMERCER[0]}
-
 done < /etc/skt.d/data/woocommerce/all-invoice.txt
 
-#count="`cat /root/export_orders_${WOOCOMMERCER[0]} | wc -l`"
-#for ((n=0;n <= $count -1; n++)); do
-
-#curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders/$n \
-#    -u consumer_key:consumer_secret
-
-
-
-
-
-
-
-
+#CHECK QUANTITY VARIANT
 while IFS=$'\t'	read -r -a WOOCOMMERCER
 do
 count="`cat /root/export_orders_${WOOCOMMERCER[0]} | wc -l`"
-for (( i=0; i <= $count-1; i++ ))
+for (( i=1; i <= $count; i++ )); do
+curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders/`sed -n "${i}p" /root/export_orders_${WOOCOMMERCER[0]}` \
+    -u "${WOOCOMMERCER[2]}" \
+	-H "Content-Type: application/json" \ | python -m json.tool | jq -r ".line_items[].sku" | cat >> /root/total_order_${WOOCOMMERCER[0]}_`sed -n "${i}p" /root/export_orders_${WOOCOMMERCER[0]}`
+done
+done < /etc/skt.d/data/woocommerce/all-invoice.txt
+
+
+
+#GET ORDER DETAILS
+while IFS=$'\t'	read -r -a WOOCOMMERCER
 do
-#for (( j=0; j <= $quantity-1; j++))
+count="`cat /root/export_orders_${WOOCOMMERCER[0]} | wc -l`"
+for (( i=0; i <= $count-1; i++ )); do
+n=`expr $i + 1`
+order_id="`sed -n "${n}p" /root/export_orders_${WOOCOMMERCER[0]}`"
+total_order="`cat /root/total_order_${WOOCOMMERCER[0]}_${order_id} | wc -l`"
+for (( j=0; j <= ${total_order}-1; j++)); do
 curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders \
     -u "${WOOCOMMERCER[2]}" \
 	-H "Content-Type: application/json" \
 	-d '{
 	"status": "processing"	
-	}' \ | python -m json.tool | jq -r "[.[$i].transaction_id,.[$i].id,.[$i].shipping.first_name,.[$i].shipping.last_name,.[$i].billing.phone,.[$i].shipping.address_1,.[$i].shipping.address_2,.[$i].shipping.city,.[$i].shipping.state,.[$i].shipping.postcode,.[$i].line_items[j].sku,.[$i].line_items[j].quantity] | @csv"| cat >> /root/orders.csv
+	}' \ | python -m json.tool | printf "\n${WOOCOMMERCER[0]}, `jq -r "[.[$i].transaction_id,.[$i].id,.[$i].shipping.first_name,.[$i].shipping.last_name,.[$i].billing.phone,.[$i].shipping.address_1,.[$i].shipping.address_2,.[$i].shipping.city,.[$i].shipping.state,.[$i].shipping.postcode,.[$i].line_items[$j].sku,.[$i].line_items[$j].quantity] | @csv"`" | cat >> /root/orders.csv	
+
+done		
 done
 done < /etc/skt.d/data/woocommerce/all-invoice.txt
 
@@ -111,8 +117,12 @@ done < /etc/skt.d/data/woocommerce/all-invoice.txt
 #WC5 url5 consumer_key5:consumer_secret5
 #WC6 url6 consumer_key6:consumer_secret6
 
-
-
+#REMOVE TRASH
+rm -rf /root/total_order_*
+rm -rf /root/export_orders/*
+clear
+#PRINT RESULTS
+cat /root/orders.csv
 
 else 
 	printf "404!! Other\n"
