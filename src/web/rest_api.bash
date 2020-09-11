1577fa4d946c8a4036c262ae895b0cbc0ffe65f7
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-09-11 12:50:06 +0700

    Update rest_api.bash

--- src/web/rest_api.bash
+++ src/web/rest_api.bash
@@ -32,13 +32,19 @@ elif [ $OPTION = 1 ]; then
 #WC5 url5 consumer_key5:consumer_secret5
 #WC6 url6 consumer_key6:consumer_secret6
 #	sh /etc/skt.d/tool/web/rest_api.bash
+
+
+
+
+
 elif [ $OPTION = 2 ]; then
 clear
-printf "IMPORTANT: ORDER IS INCLUDED IMPORT TO WOOCOMMERCER AND PAYPAL ALSO\n" 
+printf "IMPORTANT: ORDER IS INCLUDED IMPORT TO WOOCOMMERCE AND PAYPAL ALSO\n" 
 printf "DO YOU WANT TO IMPORT TRACKING NUMBER? - (Y/N): "
 read QUESTION
 if [ $QUESTION = Y -o $QUESTION = y ]; then
 #READ FILE track.txt then find REST API
+#####                 WOOCOMMERCE
 while IFS=$'\t'	read -r -a TRACK
 do
 	#spilit invoice name and invoice number, example WC1-21
@@ -51,6 +57,7 @@ do
 	  "tracking_number": "'${TRACK[3]}'"
 	  
 	}' \ | python -m json.tool
+	
 	curl -X PUT https://`sed -n '1p' /etc/skt.d/data/woocommerce/API_${INVOICE[0]}`/wp-json/wc/v3/orders/${INVOICE[1]} \
     -u "`sed -n '2p' /etc/skt.d/data/woocommerce/API_${INVOICE[0]}`" \
     -H "Content-Type: application/json" \
@@ -67,20 +74,21 @@ sleep 3
 printf "UPDATE: IMPORTED ORDER TO WOOCOMMERCE\n"
 sleep 5
 
-
-#FULFILLMENT ORDERS TO PAYPAL
+#####                 PAYPAL
 printf " ###############################\n"
 printf "     FULFILLMENT | PAYPAL API   \n"
 printf " ###############################\n"
 
 #UPDATE ALL ACCESS TOKEN
-while IFS= read -r line; do
-curl -v POST https://api.paypal.com/v1/oauth2/token \
+while IFS= read -r VPS; do
+curl -v POST "https://api.paypal.com/v1/oauth2/token" \
   -H "Accept: application/json" \
   -H "Accept-Language: en_US" \
-  -u "`sed -n "1p" /etc/skt.d/data/paypal/API/${line}_clientid_secret_key`" \
-  -d "grant_type=client_credentials" \ | python -m json.tool | printf `jq  -r ".access_token"` | cat > /etc/skt.d/data/paypal/token/${line}_access_token
+  -u "`sed -n '1p' /etc/skt.d/data/paypal/API/${VPS}_clientid_secret_key`" \
+  -d "grant_type=client_credentials" \ | python -m json.tool | printf `jq -r ".access_token"` | cat > /etc/skt.d/data/paypal/token/${VPS}_access_token
 done < /etc/skt.d/data/paypal/vps.txt
+
+sleep 3
 
 #FULFIL TRACKING
 while IFS=$'\t' read -r -a TRANSACTION
@@ -101,14 +109,17 @@ curl -v -X POST https://api.paypal.com/v1/shipping/trackers-batch \
   ]
 }' \ | python -m json.tool | printf "Đã ép thành công transaction id: `jq -r ".tracker_identifiers[].transaction_id"`: ${TRANSACTION[2]}\n"
 done < /root/track.txt
+
+
+
+
 #FORMAT track.txt file
 #E4 8MC585209K746392H WC5-27672 9400109205568128990983 USPS
 #E5 8MC585209K746392H WC5-27672 9400109205568743137961 USPS
 clear
 printf "UPDATE: IMPORTED ORDER TO PAYPAL\n"
-sleep 2
-printf "DONE!!!\n"
 sleep 1
+printf "DONE!!!\n"
 elif [ $QUESTION = N -o $QUESTION = n ]; then
 printf "STATUS: CANCEL UPDATE\n"
 sleep 1
@@ -124,45 +135,38 @@ printf " #######################################\n"
 printf " Export Orders | REST API | WOOCOMMERCE\n"
 printf " #######################################\n"
 
+
+  | jq -r ".[].id" | cat >> /root/export_orders_${WOOCOMMERCE[0]}
 #GET NUMBER OF ORDERS
-while IFS=$'\t'	read -r -a WOOCOMMERCER
+while IFS=$'\t' read -r -a WOOCOMMERCE 
 do
-curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders \
-    -u "${WOOCOMMERCER[2]}" \
-	-H "Content-Type: application/json" \
-	-d '{
-	"status": "processing"	
-	}' \ | python -m json.tool | jq -r ".[].id" | cat > /root/export_orders_${WOOCOMMERCER[0]}
+curl -X GET "https://${WOOCOMMERCE[1]}/wp-json/wc/v3/orders?status=processing&per_page=100" \
+    -u "${WOOCOMMERCE[2]}" | python -m json.tool
 done < /etc/skt.d/data/woocommerce/all-invoice.txt
 
 #CHECK QUANTITY VARIANT
-while IFS=$'\t'	read -r -a WOOCOMMERCER
+while IFS=$'\t'	read -r -a WOOCOMMERCE
 do
-count="`cat /root/export_orders_${WOOCOMMERCER[0]} | wc -l`"
+count="`cat /root/export_orders_${WOOCOMMERCE[0]} | wc -l`"
 for (( i=1; i <= $count; i++ )); do
-curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders/`sed -n "${i}p" /root/export_orders_${WOOCOMMERCER[0]}` \
-    -u "${WOOCOMMERCER[2]}" \
-	-H "Content-Type: application/json" \ | python -m json.tool | jq -r ".line_items[].sku" | cat >> /root/total_order_${WOOCOMMERCER[0]}_`sed -n "${i}p" /root/export_orders_${WOOCOMMERCER[0]}`
+curl -X GET "https://${WOOCOMMERCE[1]}/wp-json/wc/v3/orders/`sed -n "${i}p" /root/export_orders_${WOOCOMMERCE[0]}`" \
+    -u "${WOOCOMMERCE[2]}" | python -m json.tool | jq -r ".line_items[].sku" | cat >> /root/total_order_${WOOCOMMERCE[0]}_`sed -n "${i}p" /root/export_orders_${WOOCOMMERCE[0]}`
 done
 done < /etc/skt.d/data/woocommerce/all-invoice.txt
 
 
 printf "INVOICE NAME,TRANSACTION ID,ORDER ID,FIRST NAME,LAST NAME,PHONE NUMBER,ADDRESS 1,ADDRESS 2,CITY,STATES,ZIPCODE, SKU,QUANTITY"  | cat >> /root/orders.csv	
 #GET ORDER DETAILS
-while IFS=$'\t'	read -r -a WOOCOMMERCER
+while IFS=$'\t'	read -r -a WOOCOMMERCE
 do
-count="`cat /root/export_orders_${WOOCOMMERCER[0]} | wc -l`"
+count="`cat /root/export_orders_${WOOCOMMERCE[0]} | wc -l`"
 for (( i=0; i <= $count-1; i++ )); do
 n=`expr $i + 1`
-order_id="`sed -n "${n}p" /root/export_orders_${WOOCOMMERCER[0]}`"
-total_order="`cat /root/total_order_${WOOCOMMERCER[0]}_${order_id} | wc -l`"
+order_id="`sed -n "${n}p" /root/export_orders_${WOOCOMMERCE[0]}`"
+total_order="`cat /root/total_order_${WOOCOMMERCE[0]}_${order_id} | wc -l`"
 for (( j=0; j <= ${total_order}-1; j++)); do
-curl -X GET https://${WOOCOMMERCER[1]}/wp-json/wc/v3/orders \
-    -u "${WOOCOMMERCER[2]}" \
-	-H "Content-Type: application/json" \
-	-d '{
-	"status": "processing"	
-	}' \ | python -m json.tool | printf "\n${WOOCOMMERCER[0]}, `jq -r "[.[$i].transaction_id,.[$i].id,.[$i].shipping.first_name,.[$i].shipping.last_name,.[$i].billing.phone,.[$i].shipping.address_1,.[$i].shipping.address_2,.[$i].shipping.city,.[$i].shipping.state,.[$i].shipping.postcode,.[$i].line_items[$j].sku,.[$i].line_items[$j].quantity] | @csv"`" | cat >> /root/orders.csv	
+curl -X GET "https://${WOOCOMMERCE[1]}/wp-json/wc/v3/orders?status=processing&per_page=100" \
+    -u "${WOOCOMMERCE[2]}" | python -m json.tool | printf "\n${WOOCOMMERCE[0]}, `jq -r "[.[$i].transaction_id,.[$i].id,.[$i].shipping.first_name,.[$i].shipping.last_name,.[$i].billing.phone,.[$i].shipping.address_1,.[$i].shipping.address_2,.[$i].shipping.city,.[$i].shipping.state,.[$i].shipping.postcode,.[$i].line_items[$j].sku,.[$i].line_items[$j].quantity] | @csv"`" | cat >> /root/orders.csv	
 
 done		
 done
