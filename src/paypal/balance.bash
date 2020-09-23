331f82166bab73055225d10ae0412c7668e83279
Fred Ngo <55904049+anonymosX@users.noreply.github.com>
2020-09-23 15:05:39 +0700

    Update balance.bash

--- src/paypal/balance.bash
+++ src/paypal/balance.bash
@@ -18,9 +18,9 @@ elif [ $OPTION = 1 ]; then
 	cat /etc/skt.d/data/paypal/vps.txt
 	printf "OPTION: "
 	read VPS
-	curl -v -X GET https://api.paypal.com/v1/reporting/balances?currency_code=USD \
+	curl -X GET "https://api.paypal.com/v1/reporting/balances?currency_code=USD" \
 	-H "Content-Type: application/json" \
-	-H "Authorization: Bearer `sed -n "1p" /etc/skt.d/data/paypal/token/${VPS}_access_token`" \ | python -m json.tool | jq -r ".balances[].total_balance.value,.balances[].available_balance.value,.balances[].withheld_balance.value" | cat > /etc/skt.d/data/paypal/balance/${VPS}_balance
+	-H "Authorization: Bearer `sed -n "1p" /etc/skt.d/data/paypal/token/${VPS}_access_token`" | python -m json.tool | jq -r ".balances[].total_balance.value,.balances[].available_balance.value,.balances[].withheld_balance.value" | cat > /etc/skt.d/data/paypal/balance/${VPS}_balance
 	clear
 	printf " * Paypal ${VPS}  Balance: \n"
 	printf "Total     : `sed -n "1p" /etc/skt.d/data/paypal/balance/${VPS}_balance` USD\n"
@@ -29,26 +29,29 @@ elif [ $OPTION = 1 ]; then
 	sh /etc/skt.d/tool/paypal/balance.bash		
 elif [ $OPTION = 2 ]; then
 	clear
-	while IFS= read -r VPS; do
-	curl -v -X GET https://api.paypal.com/v1/reporting/balances?currency_code=USD \
+	while IFS= read -r VPS
+	do
+	curl -X GET "https://api.paypal.com/v1/reporting/balances?currency_code=USD" \
 	-H "Content-Type: application/json" \
-	-H "Authorization: Bearer `sed -n "1p" /etc/skt.d/data/paypal/token/${VPS}_access_token`" \ | python -m json.tool | jq -r ".balances[].available_balance.value,.balances[].withheld_balance.value" | cat > /etc/skt.d/data/paypal/balance/${VPS}_balance
+	-H "Authorization: Bearer `sed -n "1p" /etc/skt.d/data/paypal/token/${VPS}_access_token`"  | python -m json.tool | jq -r ".balances[].available_balance.value,.balances[].withheld_balance.value" | cat > /etc/skt.d/data/paypal/balance/${VPS}_balance
 	done < /etc/skt.d/data/paypal/vps.txt
 	clear
-	printf "     VPS  |   Available  |  Hold \n"
-	while IFS= read -r VPS; do
-	printf "   * Paypal ${VPS}: `sed -n "1p" /etc/skt.d/data/paypal/balance/${VPS}_balance` + `sed -n "2p" /etc/skt.d/data/paypal/balance/${VPS}_balance` USD\n"
-	printf " --------------------------------\n"
+	printf "VPS  |   Available  |  Hold \n"
+	while IFS= read -r VPS
+	do
+	printf " + Paypal ${VPS} | `sed -n "1p" /etc/skt.d/data/paypal/balance/${VPS}_balance` | `sed -n "2p" /etc/skt.d/data/paypal/balance/${VPS}_balance` USD\n"
+	printf " \n"
 	done < /etc/skt.d/data/paypal/vps.txt
 	sh /etc/skt.d/tool/paypal/balance.bash
 elif [ $OPTION = 3 ]; then
 	#UPDATE ALL ACCESS TOKEN
-	while IFS= read -r line; do
-	curl -v POST https://api.paypal.com/v1/oauth2/token \
+	while IFS= read -r line
+	do
+	curl -X POST "https://api.paypal.com/v1/oauth2/token" \
 	-H "Accept: application/json" \
 	-H "Accept-Language: en_US" \
 	-u "`sed -n "1p" /etc/skt.d/data/paypal/API/${line}_clientid_secret_key`" \
-	-d "grant_type=client_credentials" \ | python -m json.tool | printf `jq '.access_token'` | cat > /etc/skt.d/data/paypal/token/${line}_access_token
+	-d "grant_type=client_credentials" | python -m json.tool | printf `jq '.access_token'` | cat > /etc/skt.d/data/paypal/token/${line}_access_token
 	sed -i 's/"//g' /etc/skt.d/data/paypal/token/${line}_access_token
 	done < /etc/skt.d/data/paypal/vps.txt
 	clear
